a471ac70dd79f37756376d6faf8cc1c76817be74
enmasson <emasson@students.kennesaw.edu>
2022-11-21 20:25:11 -0500

    Added to GolfFitter program
    Created an initial function to start the program that greets user and starts to gather information

---
+++ GolfFitter.py
@@ -0,0 +1,14 @@
+def get_golfer_info():
+    #create intro
+    print("Welcome to this At Home Golf Fitter program!")
+    print("This program will ask some questions to gain info about you and your swing.")
+    print("Then it will determine a good set of clubs for you and give you info about them.")
+    input("Press enter to start!")
+
+    budget = int(float((input("What is the most you want to spend on a set of clubs? (Please enter an integer or dollar amount)")).strip("$")))
+    
+
+    
+    return
+
+get_golfer_info()
